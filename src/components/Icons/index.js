328c31b30dc736e4c144e79c2dfbc9993db9b423
Mehmet Ali Özdemir <mehmetaliozdemir@Mehmets-MacBook-Air.local>
2024-01-02 19:05:33 +0300

    feat uninstall font awesome and add custom icons

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,35 +1,34 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faArchive,
-  faSuitcase,
-  faSackDollar,
-} from "@fortawesome/free-solid-svg-icons";
+import { ReactComponent as Bag } from "../../assets/bag.svg";
+import { ReactComponent as Barrel } from "../../assets/barrel.svg";
+import { ReactComponent as Box } from "../../assets/box.svg";
+import { ReactComponent as Bulk } from "../../assets/bag.svg";
+import { ReactComponent as Pipe } from "../../assets/pipe.svg";
+import { ReactComponent as Sack } from "../../assets/sack.svg";
+import "./style.css";
 
 const Icons = ({ iconName }) => {
   const selectedIcons = () => {
-    return iconName === "box"
-      ? faArchive
-      : iconName === "bigbags"
-      ? faSuitcase
-      : iconName === "sacks"
-      ? faSackDollar
-      : iconName === "barrels"
-      ? faSackDollar
-      : iconName === "roll"
-      ? faSackDollar
-      : iconName === "pipes"
-      ? faSackDollar
-      : iconName === "bulk"
-      ? faSackDollar
-      : "";
+    return iconName === "box" ? (
+      <Box />
+    ) : iconName === "bigbags" ? (
+      <Bag />
+    ) : iconName === "sacks" ? (
+      <Sack />
+    ) : iconName === "barrels" ? (
+      <Barrel />
+    ) : iconName === "roll" ? (
+      <Barrel />
+    ) : iconName === "pipes" ? (
+      <Pipe />
+    ) : iconName === "bulk" ? (
+      <Bulk />
+    ) : (
+      <Box />
+    );
   };
 
-  return (
-    <>
-      <FontAwesomeIcon icon={selectedIcons()} />
-    </>
-  );
+  return <div className="icons">{selectedIcons()}</div>;
 };
 
 export default Icons;
