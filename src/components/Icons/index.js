944efe549130a6670bc0293a9316d0e880492063
Mehmet Ali Özdemir <mehmetaliozdemir@Mehmets-MacBook-Air.local>
2024-01-02 20:29:14 +0300

    fix: fix icon name typo

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { ReactComponent as Bag } from "../../assets/bag.svg";
 import { ReactComponent as Barrel } from "../../assets/barrel.svg";
 import { ReactComponent as Box } from "../../assets/box.svg";
-import { ReactComponent as Bulk } from "../../assets/bag.svg";
+import { ReactComponent as Bulk } from "../../assets/bulk.svg";
 import { ReactComponent as Pipe } from "../../assets/pipe.svg";
 import { ReactComponent as Sack } from "../../assets/sack.svg";
 import "./style.css";
